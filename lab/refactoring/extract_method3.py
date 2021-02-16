005b13e65237e6c766592eb043bcd229a548a2ea
kmurata798 <kmurata798@gmail.com>
2021-02-16 10:26:29 -0800

    pulling

--- lab/refactoring/extract_method3.py
+++ lab/refactoring/extract_method3.py
@@ -1,20 +1,26 @@
 # Written by Kamran Bigdely
 # Example for Compose Methods: Extract Method.
 import math
+
+def calc_distance(xa, ya, xb, yb):
+    return math.sqrt((xa-xb)(xa-xb) + (ya-yb)(ya-yb))
 xc1 = 4
 yc1 = 4.25
 
 xc2 = 53
 yc2 = -5.35
+
 # Calculate the distance between the two circle
-distance = math.sqrt((xc1-xc2)**2 + (yc1 - yc2)**2)
+distance = calc_distance(xc1, yc1, xc2, yc2)
 print('distance', distance)
+
 # *** somewhere else in your program ***
 xa = -36
 ya = 97
 
 xb = .34
 yb = .91
-# calcualte the length of vector AB vector which is a vector between A and B points.
-length = math.sqrt((xa-xb)*(xa-xb) + (ya-yb)*(ya-yb))
+
+# calculate the length of vector AB vector which is a vector between A and B points.
+length = calc_distance(xa, ya, xb, yb)
 print('length', length)
--- lab/refactoring/replace_temp_with_query.py
+++ lab/refactoring/replace_temp_with_query.py
@@ -5,6 +5,7 @@
 class Employer:    
     def __init__(self, name):
         self.name = name
+        
     def send(self, students):
         print("Students' contact info were sent to", self.name + '.')
 
@@ -12,14 +13,28 @@ class Student:
     def __init__(self, gpa, name):
         self.gpa = gpa
         self.name = name
+
     def get_gpa(self):
         return self.gpa
+
     def send_congrat_email(self):
         print("Congrats", self.name + ". You graduated successfully!")
 
 class School:
     def __init__(self, students) -> None:
         self.students = students
+
+    def calculate_top10_percent(self, passed_students):
+        passed_students.sort(key=lambda s: s.get_gpa())
+        percentile = 0.9
+        index = int(percentile * len(passed_students))
+        return passed_students[index:]
+
+    def send_top10_percent(self, passed_students):
+        top_employers = [Employer('Microsoft'), Employer('Free Software Foundation'), Employer('Google')]
+        for e in top_employers:
+            e.send(calculate_top10_percent(passed_students))
+
     def process_graduation(self):
         # Find the students in the school who have successfully graduated.
         min_gpa = 2.5 # minimum acceptable GPA
@@ -37,13 +52,7 @@ class School:
         for s in passed_students:
             s.send_congrat_email()
         # Find the top 10% of them and send their contact to the top employers
-        passed_students.sort(key=lambda s: s.get_gpa())
-        percentile = 0.9
-        index = int(percentile * len(passed_students))
-        top_10_percent = passed_students[index:]
-        top_employers = [Employer('Microsoft'), Employer('Free Software Foundation'), Employer('Google')]
-        for e in top_employers:
-            e.send(top_10_percent)
+        send_top10_percent(passed_students)
 
 students = [Student(2.1, 'donald'), Student(2.3, 'william'), Student(2.7, 'toro'), 
             Student(3.9, 'lili'), Student(3.2,'kami'), Student(3,'sarah')]
