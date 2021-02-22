de43f1e3d7572015df64cead67c1e85edbe2e522
kmurata798 <kmurata798@gmail.com>
2021-02-21 18:09:25 -0800

    consolidate_dup_conditional_frag done

--- lab/refactoring/consolidate_duplicate_conditional_fragments.py
+++ lab/refactoring/consolidate_duplicate_conditional_fragments.py
@@ -1,5 +1,6 @@
 # by Kami Bigdely
 # Consolidate duplicate conditional fragments
+
 def add(mix, something):
     mix.append(something)
     return mix
@@ -9,15 +10,13 @@ def mixer_ice_with_cream():
     return ['ice', 'cream']
 
 def make_drink(drink, addons):
+    mix = []
     if 'coffee' in drink:
-        mix = []
         mix = add(mix, 'coffee')
-        mix = add(mix, addons)
     if 'strawberry milkshake' in drink:
-        mix = []
         mix = mixer_ice_with_cream()
         mix = add(mix, 'strawberry')
-        mix = add(mix, addons)
+    mix = add(mix, addons)
     return mix
 
 final_drink = make_drink('strawberry milkshake', ['milk','sugar'])
