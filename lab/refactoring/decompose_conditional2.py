394bcbdce1f2323b2e0376d7ac585c41c2103fe5
kmurata798 <kmurata798@gmail.com>
2021-02-21 17:48:26 -0800

    decompose_conditional2 done

--- lab/refactoring/decompose_conditional2.py
+++ lab/refactoring/decompose_conditional2.py
@@ -1,21 +1,28 @@
 # By Kami Bigdely
 # Decompose conditional
 # Reference: https://www.healthline.com/health/high-cholesterol/levels-by-age
+def good_condition(total_cholostrol, ldl, triglyceride):
+    return total_cholostrol < 200 and ldl < 100 and triglyceride < 150
 
+def high_condition(total_cholostrol, ldl, triglyceride):
+    return 200 < total_cholostrol > 240 or ldl > 160 or triglyceride >= 200
+
+def borderline_condition(total_cholostrol, ldl, triglyceride):
+    return 200 < total_cholostrol < 240 or 130 < ldl < 160 or 150 <= triglyceride < 200
 # Blood test analysis program
 total_cholostrol = 70
 ldl = 30
 triglyceride = 120
 
-if total_cholostrol < 200 and ldl < 100 and triglyceride < 150:
+if good_condition(total_cholostrol, ldl, triglyceride) == True:
     # good level
     print('*** Good level of cholestrol ***')
-elif 200 < total_cholostrol > 240 or ldl > 160 or triglyceride >= 200:
+elif high_condition(total_cholostrol, ldl, triglyceride) == True:
     # High cholestrol level
     print('*** High cholestrol level ***')
     print('start taking pills such as statins')
     print('start TLC diet')
-elif 200 <total_cholostrol < 240 or 130 < ldl < 160 or 150 <= triglyceride < 200:
+elif borderline_condition(total_cholostrol, ldl, triglyceride) == True:
     #TLC_diet
     print('*** Borderline to moderately elevated ***')
     print("Start TLC diet")
@@ -25,3 +32,4 @@ elif 200 <total_cholostrol < 240 or 130 < ldl < 160 or 150 <= triglyceride < 200
     print('fruits such as apples, pears, bananas, and oranges.')
 else:
     print('Error: unhandled case.')
+
