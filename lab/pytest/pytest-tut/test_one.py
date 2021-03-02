5fb8ef96f613bd3e45367accca98b96595624b80
kmurata798 <kmurata798@gmail.com>
2021-03-02 09:54:25 -0800

    pulling upstream final

---
+++ lab/pytest/pytest-tut/test_one.py
@@ -0,0 +1,13 @@
+# test_one.py
+
+def calculate_kinetic_energy(mass, velocity): 
+    """Returns kinetic energy of mass [kg] with velocity [ms]."""
+    # Passes test
+    # return 0.5 * mass * velocity ** 2
+    # Fails test
+    return mass * velocity ** 2
+
+def test_calculate_kinetic_energy():
+    mass = 10 # [kg]
+    velocity = 4 # [m/s]
+    assert calculate_kinetic_energy(mass, velocity) == 80
